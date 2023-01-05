326f69e6375858588d3752954f6ae61f7ea08ce4
Jason Shannon <jwshannon@gmail.com>
2023-01-05 13:04:26 -0700

    made the dark theme the default

--- src/hooks/useThemeSwitcher.js
+++ src/hooks/useThemeSwitcher.js
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 const useThemeSwitcher = () => {
 	const [theme, setTheme] = useState(localStorage.theme);
 	const activeTheme = theme === 'dark' ? 'light' : 'dark';
+	
 
 	useEffect(() => {
 		const root = window.document.documentElement;
 
 		root.classList.remove(activeTheme);
 		root.classList.add(theme);
-		localStorage.setItem('theme', theme);
+		localStorage.setItem('theme', 'dark');
 	}, [theme, activeTheme]);
 
 	return [activeTheme, setTheme];
