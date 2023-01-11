175af98e9d8cbe0f324b691e164849f5dbe879a5
Jason Shannon <jwshannon@gmail.com>
2023-01-11 14:32:23 -0700

    Updated the link to my resume

--- src/components/shared/AppBanner.js
+++ src/components/shared/AppBanner.js
@@ -49,7 +49,7 @@ const AppBanner = () => {
 					className="flex justify-center sm:block">
 					<a
 						download="Jason-Shannon-Resume.pdf"
-						href="https://drive.google.com/file/d/1jDlF3Zndg2cogExMr1oeuiTIFPxrFH9y/view?usp=sharing"
+						href="https://drive.google.com/file/d/1ojhQDThu2Homy1vh5dj7Y6Iql1jyAt1W/view?usp=sharing"
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg cursor-pointer py-2.5 sm:py-3 shadow-lg rounded-lg bg-green-500 hover:bg-green-600 text-white hover:text-white duration-500  "
 						aria-label="My Resume"
 					>
