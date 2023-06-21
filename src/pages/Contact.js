b08a5bdfb0a5d06397f1fa3872d45d77b786b01f
JaiDoubleU <jwshannon@gmail.com>
2023-06-21 16:39:22 -0600

    fixed an unused var error

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 import ContactDetails from '../components/contact/ContactDetails';
-import ContactForm from '../components/contact/ContactForm';
 
 const Contact = () => {
 	return (
